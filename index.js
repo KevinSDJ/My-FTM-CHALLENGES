47a27c6fcb89704b6752cc6e28a330bf1b2873b4
kevin De Jesus <sebadjkevin@gmail.com>
2022-07-19 19:51:42 -0300

    server configure headers

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ const helmet= require('helmet')
 app.use(morgan('dev'))
 app.disable('x-powered-by')
 app.use(helmet())
-
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin','*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE,HEAD');
+  next();
+});
 // set views
 app.set("view engine", "ejs"); // express app uses ejs as view engine
 //app.set("views", path.join(__dirname, "views")); // express app uses ejs files in "views" folder to render html
